2721d9f50fd01f43adb8aefaf2adebd2c9dd2f7f
1999ADEK <b06201018@ntu.edu.tw>
2019-12-27 14:23:58 +0800

    Modify filepath

--- get_points.py
+++ get_points.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 17 09:33:27 2019
-
-@author: ADEK
-"""
-
 import matplotlib.pyplot as plt
 
-file_name = input("Filename:")
+file_name = input("What is the name of the file:")
 
 fig, ax = plt.subplots()
 ax.axis('equal')
 pts = plt.ginput(-1, 0)
 n = len(pts)
 
-with open(file_name, 'w+') as f:
+file_path = 'points/'
+with open(file_path + file_name, 'w+') as f:
     f.write('%d\n' % n)
     for (x, y) in pts:
         f.write('%f %f\n' % (x, y))
